a229b18c7b885ea810b9c138eed4d4a598339b08
dellacer2003 <khoadangnguyen1810@gmail.com>
2024-04-20 17:34:01 +0700

    sixth wireframe

--- assets/script/questions.js
+++ assets/script/questions.js
@@ -98,7 +98,7 @@ var question10 = {
 
 // Khai báo biến global để lưu trữ số thứ tự của câu hỏi hiện tại
 let currentQuestionNumber = 1;
-let currentProgress = 1; // lệch 1 -> tí cái thanh nó kh có đồng bộ 
+let currentProgress = 1;
 
 // Lấy ra thẻ div chứa câu hỏi và các lựa chọn
 const questionDiv = document.getElementById("question");
@@ -110,8 +110,8 @@ const option3 = document.getElementById("option3");
 function updateQuestion(questionNumber) {
   const currentQuestion = window["question" + questionNumber];
   // Sử dụng số thứ tự của câu hỏi trong nội dung câu hỏi
-  console.log(questionNumber)
-  console.log(currentQuestion)
+  console.log(questionNumber); //Linh's code
+  console.log(currentQuestion); //Linh's code
   questionDiv.innerText = `Question ${questionNumber}: ${currentQuestion.question}`;
   option1.innerText = currentQuestion.options[0];
   option2.innerText = currentQuestion.options[1];
@@ -130,7 +130,8 @@ updateProgress(currentProgress);
 const options = document.querySelectorAll(".option");
 options.forEach((option) => {
   option.addEventListener("click", () => {
-    if (currentQuestionNumber < 10) { //nếu bé hơn 11 thì đọc ra ngoài mảng mất :D 
+    if (currentQuestionNumber < 10) {
+      //nếu bé hơn 11 thì đọc ra ngoài mảng mất :D
       currentQuestionNumber++;
     } else return;
     if (currentProgress < 10) {
@@ -143,12 +144,11 @@ options.forEach((option) => {
 
 // Lấy ra thẻ có id là "start-again-btn"
 const startAgainBtn = document.getElementById("start-again-btn");
-
 // Thêm sự kiện "click" vào thẻ
 startAgainBtn.addEventListener("click", () => {
   // Thiết lập lại số thứ tự của câu hỏi về 1
-  currentQuestionNumber = 0;
-  currentProgress = 0;
+  currentQuestionNumber = 1;
+  currentProgress = 1;
   // Cập nhật lại câu hỏi và các lựa chọn cho câu hỏi đầu tiên
   updateQuestion(currentQuestionNumber);
   // Cập nhật lại progress bar khi bắt đầu lại từ đầu
